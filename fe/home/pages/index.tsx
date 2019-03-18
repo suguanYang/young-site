04225e1133a5682143e0d4cc6298711b2cbcd305
Donnie Wang <donnie.wang@striking.ly>
2019-03-18 19:20:49 +0800

    judgement path

--- fe/home/pages/index.tsx
+++ fe/home/pages/index.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react'
-import { render  } from 'react-dom'
+import * as React from "react"
+import { render } from "react-dom"
 
 class Index extends React.Component<any, any> {
   constructor(props: any) {
@@ -14,5 +14,5 @@ class Index extends React.Component<any, any> {
 }
 
 render(
-  <Index />,  document.querySelector('#app')
+  <Index />,  document.querySelector("#app"),
 )
